53f4690c9c497723b89cf5670e1504757a2164aa
missbreadsuperman <missbreadupserman@gmail.com>
2019-03-24 15:58:35 +0800

    Updates

---
+++ precache-manifest.bd878bf73c64ed4c3757aa5f861b3657.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "332df7ebcee07787cd83c428ec067744",
+    "url": "/personalPage/static/media/workout.332df7eb.JPG"
+  },
+  {
+    "revision": "d2a2c2598df0bebd6d0ee71c604c9d1d",
+    "url": "/personalPage/static/media/picture.d2a2c259.jpg"
+  },
+  {
+    "revision": "74bcfbe0cea22b9e4056a485719451c9",
+    "url": "/personalPage/static/media/guitar.74bcfbe0.jpg"
+  },
+  {
+    "revision": "7bec86f342e2dad8e5550a16c459e2b2",
+    "url": "/personalPage/static/media/beach.7bec86f3.jpg"
+  },
+  {
+    "revision": "1258df4a0d436bba4615c60b4056ee60",
+    "url": "/personalPage/static/media/basketball.1258df4a.jpg"
+  },
+  {
+    "revision": "596df3cbd61490a8cba2",
+    "url": "/personalPage/static/js/runtime~main.57f0ba46.js"
+  },
+  {
+    "revision": "03426036808f0633244e",
+    "url": "/personalPage/static/js/main.cc0f9533.chunk.js"
+  },
+  {
+    "revision": "39565589437f0e94d7cf",
+    "url": "/personalPage/static/js/2.dbe92f68.chunk.js"
+  },
+  {
+    "revision": "03426036808f0633244e",
+    "url": "/personalPage/static/css/main.da5f398d.chunk.css"
+  },
+  {
+    "revision": "fe885980ef454ee6c7e64b1716e9ea20",
+    "url": "/personalPage/index.html"
+  }
+];
